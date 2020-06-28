9d2ffa51a47dda980e0c70b5a927917492e54078
unknown <pimpale2003@gmail.com>
2020-06-28 11:02:05 -0700

    arm

--- POR/test/object_detection_caffe_model/object_detect.py
+++ POR/test/object_detection_caffe_model/object_detect.py
@@ -18,7 +18,7 @@ args = vars(parser.parse_args())
 
 # Initialize classes list and generate bounding box colors
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
-           "diningtable","dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+           "diningtable","dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor", "cellphone"]
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES)))
 
 # Load model
