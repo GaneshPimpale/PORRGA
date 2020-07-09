c77165cb8f403f700114ed8a1468dcac5f6eac4d
Evan Huang <ehuang1126@gmail.com>
2020-07-09 14:17:53 -0700

    fixed color channels

--- POR/test/object_detection_cvlib/cvlibTest.py
+++ POR/test/object_detection_cvlib/cvlibTest.py
@@ -6,9 +6,15 @@ from cvlib.object_detection import draw_bbox
 class cvlibTest:
 
     def object_detect(image):
+        #read in image
         img = cv2.imread(image)
+        #convert BGR to RGB
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        #detect objects
         bbox, label, conf = cv.detect_common_objects(img)
+        #display labels
         output_image = draw_bbox(img, bbox, label, conf)
+        #display output image
         plt.imshow(output_image)
         plt.show()
 
