1cce72986c3dcca6d4365d81b4f0584bcaac12de
Joanne Hui <joannehui@Joannes-MacBook-Air.local>
2020-07-14 03:12:06 -0700

    updated data collection code (needs work)

--- simulation_files/resize.py
+++ simulation_files/resize.py
@@ -1,65 +1,56 @@
-# import SolidPython package
-from solid.import *
+# import packages: SolidPython
+from solid import *
 from solid.utils import *
 from random import randint
 
+
 class Resize:
+
+    def __init__(self, ):
+        # cone/cylinder parameters
+        self.r1 = 0
+        self.r2 = 0
+        self.h = 0
+        # rectangular prism parameters (height declared above)
+        self.w = 0
+        self.l = 0
+        # ellipse parameters (height declared above)
+        self.r = 0
+
     # import OpenScad code (file name corresponds to shape)
-    scadFile = import_scad('/path/to/example.scad')
+    scad_file = import_scad('/path/to/example.scad')
+
     # classify shape + parameters
-    if scadFile == 'cone.scad':
-        def __init__(self, shape, r1, r2, h):
-            self.shape = shape
-            self.r1 = r1
-            self.r2 = r2
-            self.h = h
-        shape = 'cone'
-    elif scadFile == 'cylinder.scad':
-        def __init__(self, shape, r1, r2, h):
-            self.shape = shape
-            self.r1 = r1
-            self.r2 = r2
-            self.h = h
-        shape = 'cylinder'
-    elif scadFile == 'rectangularPrism.scad':
-        def __init__(self, shape, h, w, l):
-            self.shape = shape
-            self.h = h
-            self.w = w
-            self.l = l
-        shape = 'r_prism'
-    else:
-        def __init__(self, shape, r, h):
-            self.shape = shape
-            self.r = r
-            self.h = h
-        shape = 'ellipse'
+    def assign(self, scad_file, param1, param2, param3):
+        if scad_file == 'cone.scad':
+            self.param1 = r1
+            self.param2 = r2
+            self.param3 = h
+        elif scad_file == 'cylinder.scad':
+            self.param1 = r1
+            self.param2 = r2
+            self.param3 = h
+        elif scad_file == 'rectangularPrism.scad':
+            self.param1 = h
+            self.param2 = l
+            self.param3 = w
+        else:
+            self.param1 = r
+            self.param2 = h
 
-    # resize parameters / shape using for loop, random
-    i = 1
-    if shape == 'cone':
-        for i in range(10):
-            r1 = randint(1, 10)
-            r2 = 0
-            h = randint(1, 10)
-            scad = scadFile.cylinder(r1, r2, h)
-    elif shape == 'cylinder':
-        for i in range(10):
-            r1 = randint(1, 10)
-            r2 = randint(1, 10)
-            h = randint(1, 10)
-            scad = scadFile.cylinder(r1, r2, h)
-    elif shape == 'r_prism'
-        for i in range(10)
-            h = randint(1, 10)
-            w = randint(1, 10)
-            l = randint(1, 10)
-            scad = scadFile.box(h, w, l)
-    else:
-        for i in range(10)
-            r = randint(1, 10)
-            h = randint(1, 10)
-            scad = scadFile.ellipsis(r, h)
+    # resize parameters
+    def inrate(self):
+        if scad_file == 'cone':
+            intertools.combinations(range(11), 3)
+            # need to set param values
+        elif scad_file == 'cylinder':
+            intertools.combinations(range(11), 3)
 
-# render to file (STL/scad)
+        elif scad_file == 'rectangularPrism.scad':
+            intertools.combinations(range(11), 3)
+
+        else:
+            intertools.combinations(range(11), 2)
+
+    # render to file (STL/scad)
     scad_render_to_file(scad, 'example.scad')
