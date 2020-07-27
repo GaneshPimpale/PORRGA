2400a3dedabd0bb6a57e6ad5597b3f4cb1eb70cf
GaneshPimpale <pimpale2003@gmail.com>
2020-07-27 01:13:07 -0700

    voxel processing frame

--- test/por_test/pyodm_test/pyodm_test.py
+++ test/por_test/pyodm_test/pyodm_test.py
@@ -4,16 +4,30 @@ from pyodm import Node
 print("Creating Node connection")
 nd = Node("localhost", 3000)
 
-print("Creating ODM task")
+print("Creating ODM task...")
 path = os.path.realpath("images")
-
 image_list = [path + "/1-1.jpg",
               path + "/1-2.jpg",
-              path + "/1-3.jpg"]
+              path + "/1-3.jpg",
+              path + "/1-4.jpg",
+              path + "/1-5.jpg",
+              path + "/1-6.jpg",
+              path + "/1-7.jpg",
+              path + "/1-8.jpg",
+              path + "/1-9.jpg",
+              path + "/1-10.jpg",
+              path + "/1-11.jpg"]
 
-task = nd.create_task(image_list, {'orthophoto-resolution': 2, 'dsm': True})
-print("Running task")
+image_list_1 = [path + "/1-1.jpg",
+                path + "/1-2.jpg",
+                path + "/1-3.jpg"]
+
+task = nd.create_task(image_list_1)
+
+print("Running task...")
 task.wait_for_completion()
+
+print("Completed")
 os.listdir(task.download_assets("results"))
 
 
