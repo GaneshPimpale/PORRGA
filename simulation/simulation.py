de981c148c4c2ca5faf9b229a114a1bc7cef10cc
GaneshPimpale <pimpale2003@gmail.com>
2020-08-02 17:21:54 -0700

    removed extra garbage

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -8,7 +8,7 @@ class simulation:
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.setGravity(0,0,-9.8)
     planeId = p.loadURDF("plane.urdf")
-    handID = p.loadURDF("C:/Users/ehuan/Desktop/SHTEM/PORRGA/simulation/Hand-Assem-By-Part-URDF.SLDASM/urdf/Hand-Assem-By-Part-URDF.SLDASM.urdf", useFixedBase=True, flags= p.URDF_USE_SELF_COLLISION)
+    handID = p.loadURDF("C:/Users/ehuan/Desktop/SHTEM/PORRGA/simulation/Hand-Assem-By-Part-URDF/urdf/Hand-Assem-By-Part-URDF.urdf", useFixedBase=True, flags= p.URDF_USE_SELF_COLLISION)
     for i in range(p.getNumJoints(handID)):
         p.enableJointForceTorqueSensor(handID, i)
     jointValues.getJointValues(handID)
